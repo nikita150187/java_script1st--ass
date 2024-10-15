d6964c676b0aaf82b913361fa532176067ca3d87
nikita150187 <nikita.taware@stud.sundsgarden.se>
2024-10-15 22:54:43 +0530

    added first java program

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log('Hello,World!');
+let Name;
+Name = 'Nikita';
+console.log(Name);
+
+
+let age;
+age = '37';
+console.log(age);
+
+let favorite;
+favorite = 'Black';
+console.log(favorite);
+
+let a, b;
+a = 30;
+b = 40;
+console.log(a+b);
+
+let x, y;
+x =70;
+y =20;
+console.log(x-y);
+
+let c, d;
+c = 20;
+d = 50;
+console.log(c*d);
+
+let e, f;
+e = 120;
+f = 20;
+
+console.log(e/f);
+
+
+
+
+
